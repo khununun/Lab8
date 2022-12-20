12ae087443f37055b19680876277a161cd99e28e
kunt <khununun2546@gmail.com>
2022-12-20 15:40:25 +0700

    Done

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x)
+{
+	if(x <= 'Z' and x >= 'A')
+	{
+		if(x == 'A')
+		{
+			x = 'Z';
+		}
+		else
+		{
+		    x--;
+		}
+	}
+	else
+	{
+		x = '0';
+	}
+	return x;
 }
 
 int main(){
